6ea1aab46816960ca054efaec21b77cc0f7dbfbb
Miguel Angel Reyes Solis <15240607@itleon.edu.mx>
2018-09-26 23:11:15 -0500

    Sistema alertizador de llegada de individuos

--- Practica_2.ino
+++ Practica_2.ino
@@ -11,14 +11,16 @@
 
 
 #include "LowPower.h" // Incluimos la libreria que nos permitira manipular el consumo de energis
-#define alarma 12     // Definimos el pin por el cual sonara la alarma al activarse la puerta
+#define alarma 3     // Definimos el pin por el cual sonara la alarma al activarse la puerta
 #define puerta 2      // Definimos el pin que simulara la puerta que en este caso se hizo mediante un reed switch
 #define foco 13       // Definimos el pin que simulara el foco que se encendera cuando la puesrta se abra
 
-void setup() {        
+
+void setup() {   
+  
   pinMode(alarma, OUTPUT); // Señalamos que el pin de la alarma sera una señal de salida digital 
   pinMode(foco, OUTPUT);    // Señalamos que el pin del foco sera una señal de salida digital 
-  attachInterrupt(digitalPinToInterrupt(2), abrePuerta, LOW); //Señalamos que cuando la puerta se abra(es decir, que el red switch se active) entonces ejecute la interrupcion de hardware abrePuerta
+  attachInterrupt(digitalPinToInterrupt(puerta), abrePuerta, LOW); //Señalamos que cuando la puerta se abra(es decir, que el red switch se active) entonces ejecute la interrupcion de hardware abrePuerta
   digitalWrite(foco, LOW);  // Inicializamos con el foco apagado  
   digitalWrite(alarma, LOW);//Inicializamos la alarma en silencio
 }
@@ -34,13 +36,24 @@ void loop() {
 
 //Definimos el metodo que servira como interrupcion de hardware para que el arduino pueda salir del modo sleep
 void abrePuerta(){
-  // Ordenamos que miestras la puerta este abierta suene la alarma y que al mismo tiempo el foco parpadee
+  // Ordenamos que miestras la puerta este abierta suene la alarma y que al mismo tiempo el foco enciende
    digitalWrite(foco, HIGH); 
-   digitalWrite(alarma, HIGH);
-   delay(10000);
-   digitalWrite(alarma, LOW);
-   digitalWrite(foco, LOW);
-   delay(10000);
+   for(int contador = 0; contador <= 255; contador += 5){
+    analogWrite(alarma, contador);
+    delay(300);
+    }
+
+   for(int contador = 255; contador >= 0; contador -=5){
+    analogWrite(alarma, contador);
+    delay(300);
+    
+    }
+    digitalWrite(foco, LOW);
+    delay(300);
+   
    }
+
+
+   
 
 
